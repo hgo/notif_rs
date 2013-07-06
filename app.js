d6f3328987cf0fc0f24724ea73d6cb2ecdf7768e
hgo <guvenozyurt@gmail.com>
2013-07-07 00:35:15 +0300

    io

---
+++ app.js
@@ -0,0 +1,60 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.post('/message/:action/:to', function (req, res) {
+  target = connections[req.params.to]    
+  console.log(req.params);
+  if (target) {
+    
+    connections[req.params.to].emit(req.params.action, req.body);
+    res.send(200);
+  }
+  else
+    res.send(404);
+});
+ 
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+}),
+ connections = {},
+ io = require('socket.io').listen(server);
+
+/********** socket.io work ***************/
+io.sockets.on('connection', function(socket) {
+  console.log('io connection');
+  socket.on('username', function(username) {
+    connections[username] = socket;
+  });
+ 
+});
---
+++ public/javascripts/io.js
@@ -0,0 +1,23 @@
+var socket = io.connect('127.0.0.1:3000');
+ 
+  // Connectivity
+  socket.emit('username', 'guven');
+ 
+  socket.on('reconnect', function () {
+    console.log('Reconnected to the server');
+    socket.emit('username', 'guven');
+  });
+ 
+  socket.on('reconnecting', function () {
+    console.log('Attempting to re-connect to the server');
+  });
+ 
+  // Custom Messages
+ 
+  socket.on('bid', function(data) {
+  console.log(data);
+    /* $('body').append("<div class='notifications'>" +
+    "Bid: Rs." + data.amount + " for " + data.customer +
+    "</div>");
+    */
+  });
