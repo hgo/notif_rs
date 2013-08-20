ed0435db4cacbfd61bb6d85dcdd3d5a37f5e07ea
hgo <guvenozyurt@gmail.com>
2013-08-20 15:18:41 +0300

    minify socket.io client js

--- web.js
+++ web.js
@@ -51,6 +51,10 @@ var server = http.createServer(app).listen(app.get('port'), function(){
  io = require('socket.io').listen(server);
  
 io.configure(function () { 
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 1);                    // reduce logging
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
